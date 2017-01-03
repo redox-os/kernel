0c8ba636f4263665dae1b0fdd0a040e4e0c724f5
Jeremy Soller <jackpot51@gmail.com>
2017-01-03 15:55:00 -0700

    Cleanup Redox repo, update Rust, remove old target

---
+++ arch/x86_64/src/device/mod.rs
@@ -0,0 +1,16 @@
+use paging::ActivePageTable;
+
+pub mod cpu;
+pub mod local_apic;
+pub mod rtc;
+pub mod serial;
+
+pub unsafe fn init(active_table: &mut ActivePageTable){
+    local_apic::init(active_table);
+    rtc::init();
+    serial::init();
+}
+
+pub unsafe fn init_ap() {
+    local_apic::init_ap();
+}
