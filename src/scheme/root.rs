4441b750cf1b64c1b76446054c6efd4e633c9092
bjorn3 <bjorn3@users.noreply.github.com>
2017-04-28 18:43:39 +0200

    Remove unnecessary change

--- src/scheme/root.rs
+++ src/scheme/root.rs
@@ -58,9 +58,9 @@ impl Scheme for RootScheme {
                 let mut schemes = scheme::schemes_mut();
                 let inner = Arc::new(UserInner::new(self.scheme_id, id, path_box.clone(), flags, context));
                 schemes.insert(self.scheme_ns, path_box, |scheme_id| {
-                        inner.scheme_id.store(scheme_id, Ordering::SeqCst);
-                        Arc::new(Box::new(UserScheme::new(Arc::downgrade(&inner))))
-                    })?;
+                    inner.scheme_id.store(scheme_id, Ordering::SeqCst);
+                    Arc::new(Box::new(UserScheme::new(Arc::downgrade(&inner))))
+                })?;
                 inner
             };
 
