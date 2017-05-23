7c4c084b4453cd2835368ca6ec8c37ebcaa4e9e5
Connor Wood <connorwood71@gmail.com>
2017-05-23 17:19:09 +0100

    Completed DefOpRegion implementation. TermArg remains unimplemented, stubbed out

--- src/acpi/aml/namedobj.rs
+++ src/acpi/aml/namedobj.rs
@@ -1,6 +1,7 @@
 use super::AmlError;
 
 use super::namestring::parse_name_string;
+use super::termlist::parse_term_arg;
 
 pub fn parse_named_obj(data: &[u8]) -> Result<(u8, usize), AmlError> {
     match parse_def_op_region(data) {
@@ -21,11 +22,11 @@ fn parse_def_op_region(data: &[u8]) -> Result<(u8, usize), AmlError> {
         _ => return Err(AmlError::AmlParseError)
     };
     
-    let offset = data[3 + name_len];
-    let len = data[4 + name_len];
+    let (offset, offset_len) = parse_term_arg(&data[3 + name_len..])?;
+    let (len, len_len) = parse_term_arg(&data[3 + name_len + offset_len..])?;
 
     println!("Operation Region Found: {}\n\tSpace: {}\n\tOffset: {}\n\tLength:{}\n",
              name, region, offset, len);
 
-    Ok((32, 5 + name_len))
+    Ok((32, 3 + name_len + offset_len + len_len))
 }
