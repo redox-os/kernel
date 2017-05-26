c556726ddf0789ce7d76d8cd41671a8c9376fdb8
Connor Wood <connorwood71@gmail.com>
2017-05-26 17:05:31 +0100

    Parse device

--- src/acpi/aml/termlist.rs
+++ src/acpi/aml/termlist.rs
@@ -42,6 +42,30 @@ pub fn parse_term_arg(data: &[u8]) -> Result<(u8, usize), AmlError> {
     parse_local_obj(data)
 }
 
+pub fn parse_object_list(data: &[u8]) -> Result<Vec<u8>, AmlError> {
+    let mut terms: Vec<u8> = vec!();
+    let mut current_offset: usize = 0;
+
+    while current_offset < data.len() {
+        println!("{} {}", data[current_offset], data[current_offset + 1]);
+        
+        let (res, len) = parse_object(&data[current_offset..])?;
+        terms.push(res);
+        current_offset += len;
+    }
+
+    Ok(terms)
+}
+
+fn parse_object(data: &[u8]) -> Result<(u8, usize), AmlError> {
+    match parse_namespace_modifier(data) {
+        Ok(res) => return Ok(res),
+        Err(AmlError::AmlParseError) => ()
+    }
+
+    parse_named_obj(data)
+}
+
 fn parse_term_obj(data: &[u8]) -> Result<(u8, usize), AmlError> {
     match parse_namespace_modifier(data) {
         Ok(res) => return Ok(res),
