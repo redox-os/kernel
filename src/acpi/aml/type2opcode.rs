250d4a268ef7ce1689dffcfd774041b4f0bb6214
Connor Wood <connorwood71@gmail.com>
2017-05-26 14:42:56 +0100

    Parsed ToBuffer

--- src/acpi/aml/type2opcode.rs
+++ src/acpi/aml/type2opcode.rs
@@ -3,6 +3,11 @@ use super::AmlError;
 use super::termlist::parse_term_arg;
 
 pub fn parse_type2_opcode(data: &[u8]) -> Result<(u8, usize), AmlError> {
+    match parse_def_to_buffer(data) {
+        Ok(res) => return Ok(res),
+        Err(AmlError::AmlParseError) => ()
+    }
+    
     match parse_def_to_hex_string(data) {
         Ok(res) => return Ok(res),
         Err(AmlError::AmlParseError) => ()
@@ -13,6 +18,17 @@ pub fn parse_type2_opcode(data: &[u8]) -> Result<(u8, usize), AmlError> {
 
 fn parse_def_to_hex_string(data: &[u8]) -> Result<(u8, usize), AmlError> {
     if data[0] != 0x98 {
+        return Err(AmlError::AmlParseError);
+    }
+
+    let (operand, operand_len) = parse_term_arg(&data[1..])?;
+    let (target, target_len) = parse_term_arg(&data[1 + operand_len..])?;
+
+    Ok((8, 1 + operand_len + target_len))
+}
+
+fn parse_def_to_buffer(data: &[u8]) -> Result<(u8, usize), AmlError> {
+    if data[0] != 0x96 {
         return Err(AmlError::AmlParseError);
     }
 
