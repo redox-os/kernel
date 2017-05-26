8b89cdbeafd1c65e52ab10c1a29f3112d271442c
Connor Wood <connorwood71@gmail.com>
2017-05-26 15:58:05 +0100

    Parsed LLess

--- src/acpi/aml/type1opcode.rs
+++ src/acpi/aml/type1opcode.rs
@@ -14,11 +14,7 @@ fn parse_def_while(data: &[u8]) -> Result<(u8, usize), AmlError> {
 
     let (pkg_length, pkg_length_len) = parse_pkg_length(&data[1..])?;
     let (predicate, predicate_len) = parse_term_arg(&data[1 + pkg_length_len..])?;
-    println!("Test");
-
     let term_list = parse_term_list(&data[1 + pkg_length_len + predicate_len .. 1 + pkg_length])?;
-    println!("Test");
-
 
     Ok((12, pkg_length + 1))
 }
