3e959ac48f40b5fd03ab9e62e9160b419c3d6828
Connor Wood <connorwood71@gmail.com>
2017-05-26 17:20:02 +0100

    Parse device

--- src/acpi/aml/namespacemodifier.rs
+++ src/acpi/aml/namespacemodifier.rs
@@ -4,12 +4,30 @@ use super::AmlError;
 use super::pkglength::parse_pkg_length;
 use super::namestring::parse_name_string;
 use super::termlist::parse_term_list;
+use super::dataobj::parse_data_ref_obj;
 
 pub fn parse_namespace_modifier(data: &[u8]) -> Result<(u8, usize), AmlError> {
     match parse_scope_op(data) {
+        Ok(res) => return Ok(res),
+        Err(AmlError::AmlParseError) => ()
+    }
+    
+    match parse_name_op(data) {
         Ok(res) => Ok(res),
         Err(AmlError::AmlParseError) => Err(AmlError::AmlParseError)
     }
+}
+
+fn parse_name_op(data: &[u8]) -> Result<(u8, usize), AmlError> {
+    if data[0] != 0x08 {
+        return Err(AmlError::AmlParseError);
+    }
+
+    let (name, name_len) = parse_name_string(&data[1..])?;
+    println!("{}", name);
+    let (data_ref_obj, data_ref_obj_len) = parse_data_ref_obj(&data[1 + name_len..])?;
+    
+    Ok((12, 1 + name_len + data_ref_obj_len))
 }
 
 fn parse_scope_op(data: &[u8]) -> Result<(u8, usize), AmlError> {
