754afe4f36bdcf6f766f2f5d1b7847598b3d9e9f
Jeremy Soller <jackpot51@gmail.com>
2017-06-13 20:42:04 -0600

    Refactor kernel mapping so that symbol table is mapped

--- src/interrupt/mod.rs
+++ src/interrupt/mod.rs
@@ -1,13 +1,12 @@
 //! Interrupt instructions
 
-use core::mem;
-
-use paging::{ActivePageTable, VirtualAddress};
-
 pub mod exception;
 pub mod ipi;
 pub mod irq;
 pub mod syscall;
+pub mod trace;
+
+pub use self::trace::stack_trace;
 
 /// Clear interrupts
 #[inline(always)]
@@ -52,34 +51,4 @@ pub unsafe fn halt() {
 #[inline(always)]
 pub fn pause() {
     unsafe { asm!("pause" : : : : "intel", "volatile"); }
-}
-
-/// Get a stack trace
-//TODO: Check for stack being mapped before dereferencing
-#[inline(never)]
-pub unsafe fn stack_trace() {
-    let mut rbp: usize;
-    asm!("" : "={rbp}"(rbp) : : : "intel", "volatile");
-
-    println!("TRACE: {:>016X}", rbp);
-    //Maximum 64 frames
-    let active_table = ActivePageTable::new();
-    for _frame in 0..64 {
-        if let Some(rip_rbp) = rbp.checked_add(mem::size_of::<usize>()) {
-            if active_table.translate(VirtualAddress::new(rbp)).is_some() && active_table.translate(VirtualAddress::new(rip_rbp)).is_some() {
-                let rip = *(rip_rbp as *const usize);
-                if rip == 0 {
-                    println!(" {:>016X}: EMPTY RETURN", rbp);
-                    break;
-                }
-                println!("  {:>016X}: {:>016X}", rbp, rip);
-                rbp = *(rbp as *const usize);
-            } else {
-                println!("  {:>016X}: GUARD PAGE", rbp);
-                break;
-            }
-        } else {
-            println!("  {:>016X}: RBP OVERFLOW", rbp);
-        }
-    }
 }
