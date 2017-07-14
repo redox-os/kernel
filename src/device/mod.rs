0b6bde7c68382ad74b7e16e17595ad5f7f4b1cde
Connor Wood <connorwood71@gmail.com>
2017-07-14 15:26:04 +0100

    Moved PIT driver into kernel

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -5,6 +5,7 @@ use syscall::io::{Pio, Io};
 pub mod cpu;
 pub mod local_apic;
 pub mod pic;
+pub mod pit;
 pub mod rtc;
 pub mod serial;
 pub mod hpet;
@@ -17,16 +18,9 @@ pub unsafe fn init(active_table: &mut ActivePageTable){
 pub unsafe fn init_noncore(active_table: &mut ActivePageTable) {
     {
         if let Some(ref hpet) = ACPI_TABLE.lock().hpet {
-            // Disable the PIT
-            // TODO: Move PIT driver to kernel, and just don't enable it in the first place if we have an HPET
-            let mut pit_cmd = Pio::<u8>::new(0x43);
-            let mut pit_c0 = Pio::<u8>::new(0x40);
-
-            pit_cmd.write(0x30);
-            pit_c0.write(0);
-            pit_c0.write(0);
-
             hpet::init(hpet, active_table);
+        } else {
+            pit::init();
         }
     }
     
