9fcaf30513d8f58cc405f14b2263f499de8fa185
Jeremy Soller <jackpot51@gmail.com>
2017-08-21 20:27:13 -0600

    Fix PIT

--- src/arch/x86_64/device/pit.rs
+++ src/arch/x86_64/device/pit.rs
@@ -7,12 +7,11 @@ pub static mut COMMAND: Pio<u8> = Pio::new(0x43);
 
 static SELECT_CHAN0: u8 = 0;
 static LOHI: u8 = 0x30;
-static MODE3: u8 = 3;
 
 static CHAN0_DIVISOR: u16 = 2685;
 
 pub unsafe fn init() {
-    COMMAND.write(SELECT_CHAN0 | LOHI | MODE3);
+    COMMAND.write(SELECT_CHAN0 | LOHI | 5);
     CHAN0.write((CHAN0_DIVISOR & 0xFF) as u8);
     CHAN0.write((CHAN0_DIVISOR >> 8) as u8);
 
