57a635933376efadaab432bb750a803f899cc517
Jeremy Soller <jeremy@system76.com>
2019-12-22 18:23:36 -0700

    Do not add newline to sys:exe

--- src/scheme/sys/exe.rs
+++ src/scheme/sys/exe.rs
@@ -4,13 +4,12 @@ use crate::context;
 use crate::syscall::error::{Error, ESRCH, Result};
 
 pub fn resource() -> Result<Vec<u8>> {
-    let mut name = {
+    let name = {
         let contexts = context::contexts();
         let context_lock = contexts.current().ok_or(Error::new(ESRCH))?;
         let context = context_lock.read();
         let name = context.name.lock();
         name.clone().into_vec()
     };
-    name.push(b'\n');
     Ok(name)
 }
