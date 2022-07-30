358abbd40f51c3ce703d822c045c64593c3d1210
Jeremy Soller <jackpot51@gmail.com>
2022-07-29 18:53:27 -0600

    Fix copy paste error and update rmm

--- src/arch/x86_64/cpuid.rs
+++ src/arch/x86_64/cpuid.rs
@@ -3,7 +3,7 @@ use raw_cpuid::{CpuId, CpuIdResult};
 pub fn cpuid() -> Option<CpuId> {
     //TODO: ensure that CPUID exists! https://wiki.osdev.org/CPUID#Checking_CPUID_availability
     Some(CpuId::with_cpuid_fn(|a, c| {
-        let result = unsafe { core::arch::x86::__cpuid_count(a, c) };
+        let result = unsafe { core::arch::x86_64::__cpuid_count(a, c) };
         CpuIdResult {
             eax: result.eax,
             ebx: result.ebx,
