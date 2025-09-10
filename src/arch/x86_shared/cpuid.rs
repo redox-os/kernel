12684721034c08afec76ca7cf47fc0a5ed47fc8c
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-09-10 18:35:36 +0200

    Move arch::x86_64::cpuid into arch::x86_shared::cpuid

--- src/arch/x86_shared/cpuid.rs
+++ src/arch/x86_shared/cpuid.rs
@@ -1,4 +1,4 @@
-use raw_cpuid::{CpuId, CpuIdResult};
+use raw_cpuid::{CpuId, CpuIdResult, ExtendedFeatures, FeatureInfo};
 
 pub fn cpuid() -> CpuId {
     // FIXME check for cpuid availability during early boot and error out if it doesn't exist.
@@ -14,4 +14,16 @@ pub fn cpuid() -> CpuId {
             edx: result.edx,
         }
     })
+}
+
+#[cfg_attr(not(target_arch = "x86_64"), expect(dead_code))]
+pub fn feature_info() -> FeatureInfo {
+    cpuid()
+        .get_feature_info()
+        .expect("x86_64 requires CPUID leaf=0x01 to be present")
+}
+
+#[cfg_attr(not(target_arch = "x86_64"), expect(dead_code))]
+pub fn has_ext_feat(feat: impl FnOnce(ExtendedFeatures) -> bool) -> bool {
+    cpuid().get_extended_feature_info().map_or(false, feat)
 }
