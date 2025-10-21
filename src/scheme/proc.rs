bd98c3cfd2b2122a555d5e9e95f2b97388d869bd
Ibuki.O <isan.ethen@gmail.com>
2025-10-21 09:10:53 +0900

    wip: Add debug prints for context kill.

--- src/scheme/proc.rs
+++ src/scheme/proc.rs
@@ -1097,12 +1097,20 @@ impl ContextHandle {
                 Ok(mem::size_of_val(&mask))
             }
             ContextHandle::Status { privileged } => {
+                println!(
+                    "KERNEL: ContextHandle::Status write received for context {}",
+                    context.read(token.token()).debug_id,
+                );
                 let mut args = buf.usizes();
 
                 let user_data = args.next().ok_or(Error::new(EINVAL))??;
 
+                println!("KERNEL: Status write raw data: {:#x}", user_data);
+
                 let context_verb =
                     ContextVerb::try_from_raw(user_data).ok_or(Error::new(EINVAL))?;
+
+                println!("KERNEL: Interpreted ContextVerb: {:?}", context_verb);
 
                 match context_verb {
                     // TODO: lwp_park/lwp_unpark for bypassing procmgr?
@@ -1142,9 +1150,13 @@ impl ContextHandle {
                         Ok(size_of::<usize>())
                     }
                     ContextVerb::ForceKill => {
+                        println!(
+                            "KERNEL: Handling ForceKill for context {}",
+                            context.read(token.token()).debug_id,
+                        );
                         if context::is_current(&context) {
                             //trace!("FORCEKILL SELF {} {}", context.read().debug_id, context.read().pid);
-
+                            println!("KERNEL: ForceKill target is current context, calling exit_this_context");
                             // The following functionality simplifies the cleanup step when detached threads
                             // terminate.
                             if let Some(post_unmap) = args.next() {
@@ -1169,6 +1181,7 @@ impl ContextHandle {
                             }
                             crate::syscall::exit_this_context(None, token);
                         } else {
+                            println!("KERNEL: ForceKill target is NOT current context. Setting being_sigkilled=true for context {}", context.read(token.token()).debug_id);
                             let mut ctxt = context.write(token.token());
                             //trace!("FORCEKILL NONSELF={} {}, SELF={}", ctxt.debug_id, ctxt.pid, context::current().read().debug_id);
                             ctxt.status = context::Status::Runnable;
