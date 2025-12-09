79dc0b7364cbc092935b6db575df4f37acb9513b
Ibuki.O <isan.ethen@gmail.com>
2025-12-10 01:11:34 +0900

    refactor: Prohibit kill other thread via OpenViaDup.

--- src/scheme/proc.rs
+++ src/scheme/proc.rs
@@ -1133,10 +1133,6 @@ impl ContextHandle {
                     ContextVerb::ForceKill => {
                         if context::is_current(&context) {
                             //trace!("FORCEKILL SELF {} {}", context.read().debug_id, context.read().pid);
-                            let debug_id = context.read(token.token()).debug_id;
-                            let pid = context.read(token.token()).pid;
-                            println!("FORCEKILL SELF {} {}", debug_id, pid);
-
                             // The following functionality simplifies the cleanup step when detached threads
                             // terminate.
                             if let Some(post_unmap) = args.next() {
@@ -1164,10 +1160,6 @@ impl ContextHandle {
                             let debug_id = context::current().read(token.token()).debug_id;
                             let mut ctxt = context.write(token.token());
                             //trace!("FORCEKILL NONSELF={} {}, SELF={}", ctxt.debug_id, ctxt.pid, context::current().read().debug_id);
-                            println!(
-                                "FORCEKILL NONSELF={} {}, SELF={}",
-                                ctxt.debug_id, ctxt.pid, debug_id
-                            );
                             ctxt.status = context::Status::Runnable;
                             ctxt.being_sigkilled = true;
                             Ok(mem::size_of::<usize>())
@@ -1237,16 +1229,7 @@ impl ContextHandle {
                             }
                             crate::syscall::exit_this_context(None, token);
                         } else {
-                            let debug_id = context::current().read(token.token()).debug_id;
-                            let mut ctxt = context.write(token.token());
-                            //trace!("FORCEKILL NONSELF={} {}, SELF={}", ctxt.debug_id, ctxt.pid, context::current().read().debug_id);
-                            println!(
-                                "FORCEKILL NONSELF={} {}, SELF={}",
-                                ctxt.debug_id, ctxt.pid, debug_id
-                            );
-                            ctxt.status = context::Status::Runnable;
-                            ctxt.being_sigkilled = true;
-                            Ok(mem::size_of::<usize>())
+                            Err(Error::new(EPERM))
                         }
                     }
                     _ => Err(Error::new(EINVAL)),
