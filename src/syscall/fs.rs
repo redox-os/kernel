934162f35a9cdab8155b3b8d3a33c48e1f131b2b
Jeremy Soller <jackpot51@gmail.com>
2026-02-11 12:12:49 -0700

    Implement stdfscall

--- src/syscall/fs.rs
+++ src/syscall/fs.rs
@@ -287,7 +287,11 @@ fn call_normal(
     };
     let scheme = scheme::get_scheme(token.token(), scheme_id)?;
 
-    scheme.kcall(number, payload, flags, metadata, token)
+    if flags.contains(CallFlags::STD_FS) {
+        scheme.kstdfscall(number, payload, flags, metadata, token)
+    } else {
+        scheme.kcall(number, payload, flags, metadata, token)
+    }
 }
 
 fn call_fdwrite(
