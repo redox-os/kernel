336eda765c1829f689e7b75fc3a443836527e47a
Akshit Gaur <akshitgaur@proton.me>
2026-03-25 21:27:01 +0530

    Change locking in wakeup_contexts

--- src/context/mod.rs
+++ src/context/mod.rs
@@ -150,20 +150,27 @@ pub fn init(token: &mut CleanLockToken) {
     }
 }
 
-pub fn wakeup_context(context_lock: &Arc<RwLock<L4, Context>>) {
-    let mut global_token = unsafe { CleanLockToken::new() };
-    let mut local_token = unsafe { CleanLockToken::new() };
+pub fn wakeup_context(context_lock: &Arc<RwLock<L4, Context>>, mut token: LockToken<L0>) {
+    let mut prio = None;
+    {
+        let mut context = context_lock.write(token.token());
 
-    let mut run_queues = run_contexts_mut(global_token.token());
-    let mut context = context_lock.write(local_token.token());
+        context.wake = None;
+        context.unblock();
 
-    context.wake = None;
-    context.unblock();
+        if !(context.status.is_runnable() && !context.running && !context.enqueued) {
+            return;
+        }
 
-    if context.status.is_runnable() && !context.running && !context.enqueued {
-        let prio = context.prio;
-        run_queues.set[prio].push_back(ContextRef(Arc::clone(context_lock)));
         context.enqueued = true;
+
+        prio = Some(context.prio);
+    }
+
+    let mut run_queues = run_contexts_mut(token);
+
+    if let Some(priority) = prio {
+        run_queues.set[priority].push_back(ContextRef(Arc::clone(context_lock)));
     }
 }
 
