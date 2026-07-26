c5e8fd1ec4f1f99def1a5f208fde85f85a0f1156
Luiz Fernando Becher de Araujo <luiz.becher.araujo@gmail.com>
2026-07-26 07:38:08 -0300

    AArch64: Unify EL0/EL1 IRQ dispatch, stop EOI'ing spurious IDs
    irq_at_el0 and irq_at_el1 each duplicated the same ack/lookup/trigger
    sequence. Merge them into one dispatch() used by both, and route
    through it in order: IPI (SGI), timer, then the generic virq trigger.
    
    Also stop calling irq_eoi on GIC IDs 1020..1023: those are the
    architecturally reserved spurious/no-interrupt values, and completing
    one means acknowledging an interrupt that was never actually asserted,
    which the previous fallback path did unconditionally for any
    untranslated IRQ number.
    
    Signed-off-by: Luiz Fernando Becher de Araujo <luiz.becher.araujo@gmail.com>

--- src/arch/aarch64/interrupt/irq.rs
+++ src/arch/aarch64/interrupt/irq.rs
@@ -1,43 +1,42 @@
-use crate::{arch::device::ROOT_IC_IDX, dtb::irqchip::IRQ_CHIP, sync::CleanLockToken};
-use core::sync::atomic::Ordering;
+use crate::{
+    arch::{
+        device::{generic_timer, irqchip},
+        ipi,
+    },
+    dtb::irqchip::IRQ_CHIP,
+    sync::CleanLockToken,
+};
 
 // use crate::percpu::PercpuBlock;
 
-unsafe fn irq_ack() -> (u32, Option<usize>) {
-    unsafe {
-        let ic = &mut IRQ_CHIP.irq_chip_list.chips[ROOT_IC_IDX.load(Ordering::Relaxed)].ic;
-        let irq = ic.irq_ack();
-        (irq, ic.irq_to_virq(irq))
+unsafe fn dispatch() {
+    let (raw_iar, hwirq, virq) = irqchip::acknowledge_root();
+    if ipi::handle(hwirq, raw_iar) {
+        return;
+    }
+
+    let mut token = unsafe { CleanLockToken::new() };
+    if generic_timer::handle(hwirq, raw_iar, &mut token) {
+        return;
+    }
+
+    if let Some(virq) = virq
+        && virq < 1024
+    {
+        unsafe { IRQ_CHIP.trigger_virq(virq as u32, &mut token) };
+    } else {
+        println!("unexpected irq num {}", hwirq);
+        // IDs 1020..1023 are special/spurious GIC values and must not be
+        // written to EOIR because no interrupt was acknowledged.
+        if hwirq < 1020 {
+            irqchip::end_root(raw_iar);
+        }
     }
 }
 
-exception_stack!(irq_at_el0, |_stack| {
-    unsafe {
-        let mut token = CleanLockToken::new();
-        let (irq, virq) = irq_ack();
-        if let Some(virq) = virq
-            && virq < 1024
-        {
-            IRQ_CHIP.trigger_virq(virq as u32, &mut token);
-        } else {
-            println!("unexpected irq num {}", irq);
-        }
-    }
-});
+exception_stack!(irq_at_el0, |_stack| { unsafe { dispatch() } });
 
-exception_stack!(irq_at_el1, |_stack| {
-    unsafe {
-        let mut token = CleanLockToken::new();
-        let (irq, virq) = irq_ack();
-        if let Some(virq) = virq
-            && virq < 1024
-        {
-            IRQ_CHIP.trigger_virq(virq as u32, &mut token);
-        } else {
-            println!("unexpected irq num {}", irq);
-        }
-    }
-});
+exception_stack!(irq_at_el1, |_stack| { unsafe { dispatch() } });
 
 /*
 pub unsafe fn irq_handler_gentimer(irq: u32) {
