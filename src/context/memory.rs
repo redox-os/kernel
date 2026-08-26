8d838c2caaff1a6d8a1a08c4f6e868847d98c6ff
R Aadarsh <raadarsh1107@outlook.com>
2026-08-26 17:18:08 +0530

    Return error on encountering non-existent node

--- src/context/memory.rs
+++ src/context/memory.rs
@@ -2596,7 +2596,7 @@ impl Table {
                     TableKind::User,
                     TheFrameAllocator(
                         // temporarily use this policy to allocate on target node
-                        NumaMemoryPolicy::FromPreferredNodes,
+                        NumaMemoryPolicy::FromPreferredNodesStrict,
                         numa::make_mask(1 << target_node),
                     ),
                 )
