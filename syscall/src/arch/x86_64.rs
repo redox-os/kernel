7fc4dee572ca7039e2637a34816e0b1fe7ddf644
4lDO2 <4lDO2@protonmail.com>
2026-09-03 11:23:25 +0200

    Clobber xmm0-xmm15 in x86_64 syscall wrapper.

--- syscall/src/arch/x86_64.rs
+++ syscall/src/arch/x86_64.rs
@@ -36,6 +36,22 @@ macro_rules! syscall {
                     )?
                     out("rcx") _,
                     out("r11") _,
+                    out("xmm0") _,
+                    out("xmm1") _,
+                    out("xmm2") _,
+                    out("xmm3") _,
+                    out("xmm4") _,
+                    out("xmm5") _,
+                    out("xmm6") _,
+                    out("xmm7") _,
+                    out("xmm8") _,
+                    out("xmm9") _,
+                    out("xmm10") _,
+                    out("xmm11") _,
+                    out("xmm12") _,
+                    out("xmm13") _,
+                    out("xmm14") _,
+                    out("xmm15") _,
                     options(nostack),
                 );
 
